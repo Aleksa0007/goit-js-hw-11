410dd89bfe4256b88d6ae6697d2ea3d39831c2e4
Aleksandra <167683180+Aleksa0007@users.noreply.github.com>
2024-09-25 23:42:57 +0300

    finish styles, add loader

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -16,16 +16,22 @@ export function refreshUI(images) {
             <a class="gallery-link" href="${image["largeImageURL"]}">
                 <img class="gallery-image" src="${image["webformatURL"]}" alt="${image["tags"]}" width=""/>
                 <div class="small-container">
-                <p>Likes <span>${image["likes"]}</span></p>
-                <p>Views <span>${image["views"]}</span></p>
-                <p>Comments <span>${image["comments"]}</span></p>
-                <p>Downloads <span>${image["downloads"]}</span></p>
+                <div class="text-div"><p class="image-info-title">Likes</p><span class="image-info-value">${image["likes"]}</span></div>
+                <div class="text-div"><p class="image-info-title">Views</p><span class="image-info-value">${image["views"]}</span></div>
+                <div class="text-div"><p class="image-info-title">Comments</p><span class="image-info-value">${image["comments"]}</span></div>
+                <div class="text-div"><p class="image-info-title">Downloads</p><span class="image-info-value">${image["downloads"]}</span></div>
                 </div>
             </a>
         </li>`)
     .join("");
     
     gallery.innerHTML = htmlMarkup;
+
+    lightbox.refresh();
+}
+
+export function clearUI() {
+    gallery.innerHTML = "";
 
     lightbox.refresh();
 }
--- src/main.js
+++ src/main.js
@@ -3,18 +3,23 @@ import "izitoast/dist/css/iziToast.min.css";
 
 import { searchImages } from "./js/pixabay-api.js";
 
-import { refreshUI } from "./js/render-function.js";
-
+import { refreshUI, clearUI } from "./js/render-function.js";
 
 const form = document.querySelector(`form`);
+const loader = document.querySelector(`.loader-container`);
 
 form.addEventListener("submit", event => {
     event.preventDefault();
 
     const searchValue = form.elements.search.value.trim();
     if(searchValue === ""){
-        return
+        return 
     }
+
+    loader.style.display = 'flex';
+
+    clearUI();
+    
 
     searchImages(searchValue)
         .then((jsonImages) => {
@@ -22,10 +27,20 @@ form.addEventListener("submit", event => {
             const images = jsonImages["hits"];
 
             console.log(images);
-            refreshUI(images);
+
+            if(images.length === 0) {
+                iziToast.info({
+                    title: "Info",
+                    message: "Sorry, there are no images matching your search query. Please try again!"
+                });
+            } else {
+                refreshUI(images);
+            }
         })
         .catch((error) => {
             console.log(error);
         })
+        .finally(() => {
+            loader.style.display = `none`;
+        })
 })
-
